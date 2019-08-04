454762ec88315a893fb031f8fce285c1407e7859
SevenTailCat <16370705+SevenTailCat@users.noreply.github.com>
2019-08-04 11:07:32 +0800

    Update user_news_api_only.py

--- newsSource/user_news_api_only.py
+++ newsSource/user_news_api_only.py
@@ -1,4 +1,4 @@
-#coding:utf-8
+# coding:utf-8
 import random
 import os
 import urllib.request
@@ -16,52 +16,55 @@ from Things_Define_MainNER_Triple.newsExtracter import Extracter
 使用User.outline()，将会返回的代码形式：
     [userid, title , permid , entity]
     其中userid为%d形式，其他均为str
-    
+
     例：[10330, '金蝶纯利增长达34%创四年同期新高', '4295871245', '金蝶']
 '''
 
-#[userid,newstitle,PermID:标题的实体]
+# [userid,newstitle,PermID:标题的实体]
 
-APItoken='dgKHpotpGLBMkYHDqYHMfqNgG0TkAGd2'
+APItoken = 'dgKHpotpGLBMkYHDqYHMfqNgG0TkAGd2'
 global newsid
+
 
 class init():
     def __init__(self):
         global newsid
-        newsid=644000
+        newsid = 644000
+
 
 class user_info_in():
-    def permid_search(self,entity_name):
-        header={
-            'X-AG-Access-Token':APItoken,
+    def permid_search(self, entity_name):
+        header = {
+            'X-AG-Access-Token': APItoken,
             # 'Accept':'application/json,text/plan,"/"',
             # 'Host':'api.thomsonreuters.com'
         }
         url_path = r'https://api.thomsonreuters.com/permid/search?q=name:'
-        url=quote(url_path+entity_name,safe=string.printable)
-        req=urllib.request.Request(url,headers=header)
-        text=urllib.request.urlopen(req)
-        text=text.read().decode('utf-8')
+        url = quote(url_path + entity_name, safe=string.printable)
+        req = urllib.request.Request(url, headers=header)
+        text = urllib.request.urlopen(req)
+        text = text.read().decode('utf-8')
         # text=BeautifulSoup(text)
-        text=json.loads(text,object_pairs_hook=OrderedDict)
-        text=text["result"]["organizations"]["entities"]
-        if(text==[]):
+        text = json.loads(text, object_pairs_hook=OrderedDict)
+        text = text["result"]["organizations"]["entities"]
+        if (text == []):
             print("False")
             return False
-        last=text[0]['@id']
-        permid=re.findall(r'https://permid.org/\d-(\d+)',last)
-        print(permid)
+        last = text[0]['@id']
+        permid = re.findall(r'https://permid.org/\d-(\d+)', last)
         return permid[0]
 
     def newsfile(self):
-        for i in range(self.newsid,800000):
-            path='股票/'
-            if(os.path.exists(path+str(i)+'.txt')):
-                self.newsid=i+1
-                return path+str(i)+'.txt'
+        for i in range(self.newsid, 800000):
+            path = '股票/'
+            if (os.path.exists(path + str(i) + '.txt')):
+                self.newsid = i + 1
+                nf = open(path + str(i) + '.txt', 'r')
+                title = nf.readlines()[0].replace('\n', '')
+                return title
         return "without newsfile"
 
-    def search_news(self,control):
+    def search_news(self, control):
         '''
         if control==1:
             返回数据库新闻
@@ -70,31 +73,29 @@ class user_info_in():
         :param control:
         :return:
         '''
-        lines=[]
-        if control==1:
-            nf=self.newsfile()
-            if(nf=="without newsfile"):
-                control=2
+        lines = []
+        if control == 1:
+            nf = self.newsfile()
+            if (nf == "without newsfile"):
+                control = 2
             else:
-                nf=open(nf,'r')
-                title=nf.readlines()[0].replace('\n','')
-                return title
-        if control==2: #可能接外部接口
+                return nf
+        if control == 2:  # 可能接外部接口
             return lines
 
-    def search_news_entity(self,news):
+    def search_news_entity(self, news):
         # entity=extracter.extractClass(self.news) #事件定义
-        entity = self.extracter.extractSubject(news) #可以批量处理
+        entity = self.extracter.extractSubject(news)  # 可以批量处理
         entity = entity[0][0]
         return entity
 
-    def outline(self,news_number=1,control=1):
-        self.news_number=news_number
-        self.news=self.search_news(control)
-        self.news_entity=self.search_news_entity(self.news)
+    def outline(self, news_number=1, control=1):
+        self.news_number = news_number
+        self.news = self.search_news(control)
+        self.news_entity = self.search_news_entity(self.news)
         # print([self.userid,self.news,self.news_entity])
-        self.permid=self.permid_search(self.news_entity)
-        return [self.userid,self.news,self.permid,self.news_entity]
+        self.permid = self.permid_search(self.news_entity)
+        return [self.userid, self.news, self.permid, self.news_entity]
 
     def write_txt(self):
         print("userid\ttitle\tis_click\tpermid:entity")
@@ -103,14 +104,15 @@ class user_info_in():
     def __init__(self):
         global newsid
         self.newsid = newsid
-        self.userid=10330
-        self.control=1
+        self.userid = 10330
+        self.control = 1
         self.extracter = Extracter()
         # self.extracter.createClassModel()
         self.extracter.createSubjectModel()
 
-if __name__  == "__main__":
+
+if __name__ == "__main__":
     init()
-    User=user_info_in()
+    User = user_info_in()
     print(User.outline())
     print(User.outline())
