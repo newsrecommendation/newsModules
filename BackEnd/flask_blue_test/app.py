5c01d80c6944e1f8d61103769c0c4474c8369b4f
Kelang_Tian <1141180644@qq.com>
2019-08-09 16:30:47 +0800

    v2.0

--- BackEnd/flask_blue_test/app.py
+++ BackEnd/flask_blue_test/app.py
@@ -1,15 +1,18 @@
 # 导入flask模块
 from flask import Flask
+from flask_cors import CORS
 
 # 创建Flask的实例app，并设置共享网页文件夹templates的访问位置
 app = Flask(__name__, template_folder='static/templates')
+CORS(app, supports_credentials = True)
+
 
 # 设置代码发生改变时，自动启动服务器
 app.debug = True
 
 # 以下两句先不写，蓝图创建后执行
-# 导入app1/views.py中创建的蓝图
+# 导入appnews/views.py中创建的蓝图
 
-from app1.views import app1
+from appnews.views import appnews
 
-app.register_blueprint(app1, url_prefix='/app1')
+app.register_blueprint(appnews, url_prefix='/appnews')
--- BackEnd/flask_blue_test/db_operate.py
+++ BackEnd/flask_blue_test/db_operate.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # 注意之后在需要运行数据库的文件中导入此文件，配置连接数据库
 
 # 导入SQLAlchemy，可操作数据库以及连接数据库
@@ -7,6 +8,10 @@ from app import app
 
 # 连接数据库（格式：'mysql+pymysql://用户名:密码@端口号/数据库名'）
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:19950314@127.0.0.1:3306/users'
+
+# 支持中文操作
+app.config['JSON_AS_ASCII'] = False
+
 # 数据库连接(生成一个数据库操作对象)
 db = SQLAlchemy(app)
 
